855c38b1335eb945ebcbe188feb1bfd210809462
dinesh-b-09 <dineshbseervi@gmail.com>
2023-08-18 10:57:58 +0530

    attempt-1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
-public class Main {
-  
-}
+public class Main
+{
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly("Initial Name");
+
+        // Error: The variable 'name' is private and cannot be accessed directly
+        // obj.name = "New Name"; // This line will result in an error
+
+        // Error: The variable 'name' is private and cannot be accessed directly
+        // System.out.println(obj.name); // This line will result in an error
+
+        // Using setter to set a new name
+        obj.setName("New Name");
+
+        // Using getter to access the name and print it
+        System.out.println(obj.getName());
+
+    }
+
